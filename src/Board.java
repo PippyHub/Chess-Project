e14e006a79c14ee0dd5a19e2642c9be2fd959be9
Piper Inns Hall <piper.innshall@gmail.com>
2023-06-07 17:00:42 +1200

    Paint board frame (error)

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
-
+import java.awt.*;
 public class Board extends JFrame{
     public Board() {
         JFrame frame = new JFrame();
-        frame.setBounds(10, 10, 512, 512);
-        JPanel panel = new JPanel()
-        {
+        this.setBounds(10, 10, 512, 512);
 
-        };
+        JPanel panel = new JPanel();
         this.add(panel);
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible (true);
+    }
+    public void paint(Graphics g){
+        boolean white = true;
+        for(int boardY = 0; boardY < 8; boardY++);{
+            for(int boardX = 0; boardX < 8; boardX++){
+                g.fillRect(boardX*64, boardY*64, 64, 64);
+            }
+            white=!white;
+        }
     }
 }
