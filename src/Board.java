23b8079516cfc5eb65a53e82405faa72654d95e0
Piper Inns Hall <piper.innshall@gmail.com>
2023-06-07 16:42:42 +1200

    Board frame working

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,15 @@
-public class Board {
+import javax.swing.*;
+
+public class Board extends JFrame{
     public Board() {
-        System.out.println("test");
+        JFrame frame = new JFrame();
+        frame.setBounds(10, 10, 512, 512);
+        JPanel panel = new JPanel()
+        {
+
+        };
+        this.add(panel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible (true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import java.swing.*;
 public class Main {
     public static void main(String[] args) {
-        //new Board();
-        Jframe frame = new JFrame();
-        frame.setBounds(10, 10, 512, 512);
-        JPanel panel = new JPanel()
-        {
-
-        };
-        frame.add(panel);
-        frame.setDefaultCloseOperation(3);
-        frame.setVisible (true);
+        new Board();
     }
 }
