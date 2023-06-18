75ca6e6a5fd99d700d875d29414cd51387649b3c
Piper Inns Hall <piper.innshall@gmail.com>
2023-06-18 22:07:16 +1200

    added kill and move

--- src/Piece.java
+++ src/Piece.java
@@ -4,7 +4,32 @@
  * @author (Piper Inns Hall)
  * @version (18/06/2023)
  */
+import java.util.LinkedList;
 public class Piece {
-    int px;
-    int py;
+    int pX;
+    int pY;
+    boolean isWhite;
+    LinkedList<Piece> ps;
+    public Piece(int pX, int pY, boolean isWhite, LinkedList<Piece> ps) {
+        this.pX = pX;
+        this.pY = pY;
+        this.isWhite = isWhite;
+        this.ps = ps;
+        ps.add(this);
+    }
+    public void move(int pX, int pY) {
+        for(Piece p: ps){
+            if(p.pX == pX && p.pY == pY) {
+                p.kill();
+            }
+        }
+        this.pX = pX;
+        this.pY = pY;
+
+    }
+
+    public void kill() {
+        ps.remove(this);
+    }
+
 }
