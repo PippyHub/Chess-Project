dc0b502f88eb8ae1c66a2e5b202d7e918034a185
Piper Inns Hall <piper.innshall@gmail.com>
2023-06-19 17:24:12 +1200

    adding menu bar

--- src/Board.java
+++ src/Board.java
@@ -6,33 +6,19 @@
  */
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.LinkedList;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Board extends JFrame implements ActionListener, MouseListener{
     public static LinkedList<Piece> ps = new LinkedList<>(); //linked list of pieces
     public static Piece selectedPiece = null;
-    Image[] images = new Image[12];
+    private Image[] images;
     final int HEADER_OFFSET = 30;
     public Board() {
-        try {
-            BufferedImage all = ImageIO.read(new File("src/ChessPiecesArray.png"));
 
-            int index = 0;
-            for (int y = 0; y < 120; y += 60) {
-                for (int x = 0; x < 360; x += 60) {
-                    images[index] = all.getSubimage(x, y, 60, 60)
-                            .getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
-                    index++;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //Load images from a file
+        Images img = new Images();
+        images = img.loadImages();
 
         pieceList();
 
@@ -49,39 +35,30 @@ public class Board extends JFrame implements ActionListener, MouseListener{
     }
 
     public void pieceList() {
-        Piece bKing = (new Piece(4, 0, true,"king", ps));
         Piece bQueen = (new Piece(3, 0, true,"queen", ps));
-        Piece bBishop1 = (new Piece(2, 0, true,"bishop", ps));
-        Piece bBishop2 = (new Piece(5, 0, true,"bishop", ps));
-        Piece bKnight1 = (new Piece(1, 0, true,"knight", ps));
-        Piece bKnight2 = (new Piece(6, 0, true,"knight", ps));
-        Piece bRook1 = (new Piece(0, 0, true,"rook", ps));
-        Piece bRook2 = (new Piece(7, 0, true,"rook", ps));
-        Piece bPawn1 = (new Piece(0, 1, true,"pawn", ps));
-        Piece bPawn2 = (new Piece(1, 1, true,"pawn", ps));
-        Piece bPawn3 = (new Piece(2, 1, true,"pawn", ps));
-        Piece bPawn4 = (new Piece(3, 1, true,"pawn", ps));
-        Piece bPawn5 = (new Piece(4, 1, true,"pawn", ps));
-        Piece bPawn6 = (new Piece(5, 1, true,"pawn", ps));
-        Piece bPawn7 = (new Piece(6, 1, true,"pawn", ps));
-        Piece bPawn8 = (new Piece(7, 1, true,"pawn", ps));
-
-        Piece wKing = (new Piece(4, 7, false,"king", ps));
         Piece wQueen = (new Piece(3, 7, false,"queen", ps));
-        Piece wBishop1 = (new Piece(2, 7, false,"bishop", ps));
-        Piece wBishop2 = (new Piece(5, 7, false,"bishop", ps));
-        Piece wKnight1 = (new Piece(1, 7, false,"knight", ps));
-        Piece wKnight2 = (new Piece(6, 7, false,"knight", ps));
-        Piece wRook1 = (new Piece(0, 7, false,"rook", ps));
-        Piece wRook2 = (new Piece(7, 7, false,"rook", ps));
-        Piece wPawn1 = (new Piece(0, 6, false,"pawn", ps));
-        Piece wPawn2 = (new Piece(1, 6, false,"pawn", ps));
-        Piece wPawn3 = (new Piece(2, 6, false,"pawn", ps));
-        Piece wPawn4 = (new Piece(3, 6, false,"pawn", ps));
-        Piece wPawn5 = (new Piece(4, 6, false,"pawn", ps));
-        Piece wPawn6 = (new Piece(5, 6, false,"pawn", ps));
-        Piece wPawn7 = (new Piece(6, 6, false,"pawn", ps));
-        Piece wPawn8 = (new Piece(7, 6, false,"pawn", ps));
+
+        Piece bKing = (new Piece(4, 0, true,"king", ps));
+        Piece wKing = (new Piece(4, 7, false,"king", ps));
+
+        for (int r = 0; r < 8; r+=7) { //rooks in link list
+            Piece bRook1 = (new Piece(r, 0, true,"rook", ps));
+            Piece wRook1 = (new Piece(r, 7, false,"rook", ps));
+
+        }
+        for (int k = 1; k < 7; k+=5) { //knights in link list
+            Piece bKnight1 = (new Piece(k, 0, true,"knight", ps));
+            Piece wKnight1 = (new Piece(k, 7, false,"knight", ps));
+        }
+        for (int b = 2; b < 6; b+=3) { //bishops in link list
+            Piece bBishop = (new Piece(b, 0, true,"bishop", ps));
+            Piece wBishop = (new Piece(b, 7, false,"bishop", ps));
+        }
+
+        for(int p = 0; p < 8; p++) { //pawns in link list
+            Piece bPawn = new Piece(p, 1, true,"pawn", ps);
+            Piece wPawn = new Piece(p, 6, false,"pawn", ps);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {}
---
+++ src/Images.java
@@ -0,0 +1,31 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Images {
+    private Image[] images;
+    public Images() {
+        images = new Image[12];
+    }
+
+    public Image[] loadImages() {
+        try {
+            BufferedImage all = ImageIO.read(new File("src/ChessPiecesArray.png"));
+
+            int index = 0;
+            for (int y = 0; y < 120; y += 60) {
+                for (int x = 0; x < 360; x += 60) {
+                    images[index] = all.getSubimage(x, y, 60, 60)
+                            .getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+                    index++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return images;
+    }
+}
